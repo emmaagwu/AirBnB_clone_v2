0afa3d96f28c01c142e792c61e00afb278400a62
Agwu Emmanuel <emmanuelaagwu89@gmail.com>
2024-04-19 09:45:49 +0100

    Add a script that routes the /hbnb, etc

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" start a Flask web application
+    listening on 0.0.0.0, port 5000
+    Routes:
+        /: display “Hello HBNB!”
+        /hbnb: display “HBNB”
+        /c/<text>: display “C ”, followed by the value of the text variable
+        /python/<text>: display “Python ”, followed by the value of
+            the text variable.
+        (replace underscore _ symbols with a space )
+    use the strict_slashes=False option
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Returns 'Hello HBNB' """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Returns 'HBNB' """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_c(text):
+    """ returns 'C' followed by the content of the text """
+    text_space = text.replace("_", " ")
+    return "C {}".format(text_space)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def show_python(text="is cool"):
+    """ returns 'Python' followed by the content of the text
+    Replaces any underscore in <text> with space
+    """
+    text_space = text.replace("_", " ")
+    return "Python {}".format(text_space)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
