512f30bf8ed6ff6fc3695e4ac9bd10cdd2cd9633
Agwu Emmanuel <emmanuelaagwu89@gmail.com>
2024-04-19 13:03:16 +0100

    updated engine

--- models/state.py
+++ models/state.py
@@ -16,7 +16,6 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City",  backref="state", cascade="delete")
 
-
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
