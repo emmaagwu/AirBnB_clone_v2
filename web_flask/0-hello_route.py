8364795369226d77a005180399462ed4a4375b39
Agwu Emmanuel <emmanuelaagwu89@gmail.com>
2024-04-19 09:22:54 +0100

    Add a script that routes the root

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" scripts start a web app to listen on 0.0.0.0 port 5000. """
+from flask import Flask
+app = Flask(__name__)
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ returns a string to the output"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
