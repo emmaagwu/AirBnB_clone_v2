71a1eddccf6e27774d676cb59e64f7150b152789
Agwu Emmanuel <emmanuelaagwu89@gmail.com>
2024-04-19 13:01:55 +0100

    updated engine

--- models/state.py
+++ models/state.py
@@ -16,17 +16,13 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City",  backref="state", cascade="delete")
 
-    @property
-    def cities(self):
-        """
-        return city instances with state_id == city.id
-        """
-        if getenv("HBNB_TYPE_STORAGE") != "db":
-            @property
-            def cities(self):
-                """Get a list of all related City objects."""
-                city_list = []
-                for city in list(models.storage.all(City).values()):
-                    if city.state_id == self.id:
-                        city_list.append(city)
-                return city_list
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
